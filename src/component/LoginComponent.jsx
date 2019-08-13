d009323265b7a8c0da5274a46f0a964c49426ec4
phuctt <ttran.phuc99@gmail.com>
2019-08-13 12:06:05 +0700

    add error username pass mess

--- src/component/LoginComponent.jsx
+++ src/component/LoginComponent.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Form, Icon, Input, Button, Checkbox, Alert } from 'antd';
 import LoginService from '../service/LoginService';
 import '../stylesheet/LoginStylesheet.css';
 import "antd/dist/antd.css";
 
 class LoginComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
     handleSubmit = e => {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
@@ -14,10 +19,16 @@ class LoginComponent extends React.Component {
               localStorage.setItem('username', values.username);
             }
             LoginService.login(values)
-                .then(() => {
-                    console.log("success");
-                })
-                .catch((e) => {
+                .then((response) => {
+					if (response.status === 200) {
+						console.log("success");
+					} else if (response.status === 401) {
+						this.setState({
+							errorMess: "Incorrect username or password!"
+						});
+					}
+                    
+                }).catch((e) => {
                     console.log(e);
                 });
           }
@@ -28,40 +39,41 @@ class LoginComponent extends React.Component {
         let storedUsername = localStorage.getItem('username') || '';
         const { getFieldDecorator } = this.props.form;
         return (
-          <Form onSubmit={this.handleSubmit} className="login-form">
-            <Form.Item>
-              {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input your username!' }],
-                initialValue: storedUsername
-              })(
-                <Input
-                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="Username"
-                />,
-              )}
-            </Form.Item>
-            <Form.Item>
-              {getFieldDecorator('password', {
-                rules: [{ required: true, message: 'Please input your Password!' }],
-              })(
-                <Input
-                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  type="password"
-                  placeholder="Password"
-                />,
-              )}
-            </Form.Item>
-            <Form.Item>
-              {getFieldDecorator('remember', {
-                valuePropName: 'checked',
-                initialValue: true,
-              })(<Checkbox>Remember username</Checkbox>)}
-              <Button type="primary" htmlType="submit" className="login-form-button">
-                Log in
-              </Button>
-            </Form.Item>
-          </Form>
-        );
+			<Form onSubmit={this.handleSubmit} className="login-form">
+				{ this.state.errorMess && <Alert message={this.state.errorMess || ''} type="error" showIcon />}
+				<Form.Item>
+				{getFieldDecorator('username', {
+					rules: [{ required: true, message: 'Please input your username!' }],
+					initialValue: storedUsername
+				})(
+					<Input
+					prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+					placeholder="Username"
+					/>,
+				)}
+				</Form.Item>
+				<Form.Item>
+				{getFieldDecorator('password', {
+					rules: [{ required: true, message: 'Please input your Password!' }],
+				})(
+					<Input
+					prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+					type="password"
+					placeholder="Password"
+					/>,
+				)}
+				</Form.Item>
+				<Form.Item>
+				{getFieldDecorator('remember', {
+					valuePropName: 'checked',
+					initialValue: true,
+				})(<Checkbox>Remember username</Checkbox>)}
+				<Button type="primary" htmlType="submit" className="login-form-button">
+					Log in
+				</Button>
+				</Form.Item>
+			</Form>
+		);
     }
 }
 
