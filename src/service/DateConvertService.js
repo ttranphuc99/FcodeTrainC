e91fec6b6e0052797520050d71fe7b3a5054b3ea
phuctt <ttran.phuc99@gmail.com>
2019-12-24 11:01:38 +0700

    fix month

--- src/service/DateConvertService.js
+++ src/service/DateConvertService.js
@@ -7,7 +7,7 @@ class DateConvert {
         hours = hours ? hours : 12; // the hour '0' should be '12'
         minutes = minutes < 10 ? '0'+minutes : minutes;
         var strTime = hours + ':' + minutes + ' ' + ampm;
-        return date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + "  " + strTime;
+        return date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear() + "  " + strTime;
     }
 
     convert(str) {
