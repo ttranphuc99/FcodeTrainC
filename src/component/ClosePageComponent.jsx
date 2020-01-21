000e45b9fb8e48024eae44c6e9f7b92bfd9c39cb
phuctt <ttran.phuc99@gmail.com>
2020-01-21 17:59:27 +0700

    change close page

--- src/component/ClosePageComponent.jsx
+++ src/component/ClosePageComponent.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import img from '../image/close.png';
 
 class ClosePageComponent extends React.Component {
     render() {
-        return <div>
-            Close
+        return <div style={{display: 'flex', justifyContent: 'center', height: '100vh', alignItems: 'center'}}>
+            <img style={{height: '80vh'}} alt="Close" src={img}/>
         </div>
     }
 }
